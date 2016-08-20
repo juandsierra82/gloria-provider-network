c10394bd3489619ffbf0fe3bdd6dd314570748ea
juandsierra82 <juandsierra82@gmail.com>
2016-08-20 01:32:30 -0400

    set up json api for rails backend

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'rails', '~> 5.0.0'
 # Use pg as the database for Active Record
 gem 'pg'
 gem 'rack-cors', :require => 'rack/cors'
+gem 'json'
+gem 'active_model_serializers','~>0.10.0.rc5'
 
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module GloriaProviderNetwork
              methods: [:get, :put, :post, :patch, :delete, :options]
       end
     end
+        config.autoload_paths += %W(#{config.root}/lib)
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
